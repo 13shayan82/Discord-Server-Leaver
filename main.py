73caade7fb6fb6a9452e32609344e05e5e5cab70
Seth <37915796+iSaluki@users.noreply.github.com>
2023-05-15 18:06:56 +0100

    implement basic ratelimit blocker

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-import requests
+import requests, time
 
 
 tokenlist = open("tokens.txt").read().splitlines()
 exceptions = open("exceptions.txt").read().splitlines()
+
+ratelimit = 0
 
 for token in tokenlist:
     try:
         print(f"\n[+] Leave all available guilds")
         guildsIds = requests.get("https://discord.com/api/v7/users/@me/guilds", headers={'Authorization': token}).json()
         for guild in guildsIds:
+            if ratelimit > 10:
+                print(f"\n[+] Preventing ratelimit. Waiting for 65 seconds.")
+                ratelimit = 0
+                time.sleep(65)
             if guild["id"] not in exceptions:
                 try:
                     requests.delete(
                         f'https://discord.com/api/v7/users/@me/guilds/'+guild['id'],
                         headers={'Authorization': token})
                     print(f"\t[!] Left this server: "+guild['name'])
+                    ratelimit += 1
                 except Exception as e:
                     print(f"\t[!] the following error occurred : {e}")
             else:
